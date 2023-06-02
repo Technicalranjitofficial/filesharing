8d931c741e316ef152737225d35d653365131e0a
technicalranjitofficial <dranjitkumar16@gmail.com>
2023-06-02 13:30:50 +0530

    sjsj

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -1,5 +1,22 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import os from "os";
+import path from "path";
+
+const fs = require("fs");
 
 export default async function handler(req,res){
-    res.json("hello")
+    // res.json("hello")
+
+    fs.readdir(path.join(process.cwd(),"/public/uploads"),(err,files)=>{
+        res.json(files);
+    })
+
+
+    // console.log(path.join(homedir,"./public"));
+
+    // res.json(process.cwd());
+
+
+
+    
 }
